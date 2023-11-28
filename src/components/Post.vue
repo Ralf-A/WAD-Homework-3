7771a437dfd31f9df3e0a5e7c9e7fe9e85c71a32
Karl Oskar Jaagosild <jaagosildkarl@gmail.com>
2023-11-28 15:58:25 +0200

    Styling changes, added profile picture

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,23 +1,32 @@
 <template>
   <li class="post-item">
     <div class="post-content">
+      <div class="post-profile">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/512px-Windows_10_Default_Profile_Picture.svg.png" class="profile-picture"/>
+        <p class="profile-name">{{ post.author }}</p>
+      </div>
       <div class="post-text">
         <p>{{ post.content }}</p>
         <div v-if="post.picture" class="post-image-container">
           <img :src="post.picture" :alt="'Image for Post ' + post.ID" class="post-image" />
         </div>
-        <p><strong>Date:</strong> {{ post.date }}</p>
-        <p><strong>Author:</strong> {{ post.author }}</p>
-        <p><strong>Likes:</strong> {{ post.likes }}</p>
-        <button @click="incrementLikes" class="like-button">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Facebook_Like_button.svg/1024px-Facebook_Like_button.svg.png"
-            alt="Like Button" />
-        </button>
+        <p class="post-info">
+          <strong>Date:</strong> {{ post.date }}
+        </p>
+        <div class="post-footer">
+          <button @click="incrementLikes" class="like-button">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Facebook_Like_button.svg/1024px-Facebook_Like_button.svg.png"
+              alt="Like Button"
+            />
+          </button>
+          <p class="likes-count">{{ post.likes }} Likes</p>
+        </div>
       </div>
     </div>
   </li>
 </template>
+
 
 <script>
 export default {
@@ -38,10 +47,42 @@ export default {
 
 <style scoped>
 .post-item {
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.37);
+  box-shadow: 0 0 10px rgba(0,0,0,0.37);
   border-radius: 8px;
   margin-bottom: 16px;
   overflow: hidden;
+  position: relative; 
+  z-index: 1;
+}
+.post-profile {
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  position: relative;
+  z-index: 1;
+}
+
+.profile-picture {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 8px;
+}
+
+.post-info {
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  margin: 0;
+}
+
+.profile-name {
+  margin: 0;
+}
+
+.post-text {
+  flex: 3;
+  padding: 16px;
 }
 
 .post-content {
@@ -50,38 +91,46 @@ export default {
   align-items: flex-start;
 }
 
-.post-text {
-  flex: 2;
-  padding: 16px;
-}
-
-.post-text p {
-  margin: 0;
-  margin-bottom: 8px;
-}
-
 .post-image-container {
-  flex: 1;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
 .post-image {
-  max-width: 500px;
-  max-height: 100%;
+  max-width: 100%;
+  max-height: 300px;
   padding: 10px;
+  border-radius: 8px;
+}
+
+.post-footer {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 16px;
 }
 
 .like-button {
+  margin-top: 8px;
   background: none;
   border: none;
   cursor: pointer;
   padding: 0;
+  position: absolute;
+  bottom: 8px;
+  left: 8px;
 }
 
 .like-button img {
   width: 30px;
   height: 30px;
+}
+
+.likes-count {
+  margin-top: 8px;
+  position: absolute;
+  bottom: 8px;
+  right: 8px;
 }
 </style>
