c4a5fd37561a6734809d09d502192fb427ab244a
Karl Oskar Jaagosild <jaagosildkarl@gmail.com>
2023-12-12 16:50:36 +0200

    Contact us page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,7 @@
         <nav>
             <router-link to="/">Log In</router-link>
             <router-link to="/main">Home</router-link>
+            <router-link to="/contact">Contact Us</router-link> <!-- Added button for Contact Us page -->
         </nav>
     </header>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,32 +2,39 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import MainView from "../views/MainView.vue";
 import SignUp from "../views/SignUp.vue";
 import LogIn from "../views/LogIn.vue";
+import ContactUs from "../views/ContactUs.vue";
 import auth from "../auth";
 
-const routes = [{
-    path: "/signup",
-    name: "SignUp",
-    component: SignUp,
-},
-{
-    path: "/login",
-    name: "LogIn",
-    component: LogIn,
-},
-{
-    path: '/',
-    name: 'main',
-    component: MainView,
-    beforeEnter: async (to, from, next) => {
-        let authResult = await auth.authenticated();
-        if (!authResult) {
-            next('/login')
-        } else {
-            next();
+const routes = [
+    {
+        path: "/signup",
+        name: "SignUp",
+        component: SignUp,
+    },
+    {
+        path: "/login",
+        name: "LogIn",
+        component: LogIn,
+    },
+    {
+        path: "/contact",
+        name: "ContactUs",
+        component: ContactUs,
+    },
+    {
+        path: "/",
+        name: "main",
+        component: MainView,
+        beforeEnter: async (to, from, next) => {
+            let authResult = await auth.authenticated();
+            if (!authResult) {
+                next('/login')
+            } else {
+                next();
+            }
         }
-    }
-},
-]
+    },
+];
 
 const router = createRouter({
     history: createWebHashHistory(),
