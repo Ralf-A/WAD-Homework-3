354491d4a99d8ae2aa1bea6c79a2914c4f3fa5ad
OPuusalu <91115628+OPuusalu@users.noreply.github.com>
2023-12-12 16:26:04 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,33 @@ import SignUpView from '../views/SignUpView.vue';
 import MainView from '../views/MainView.vue';
 
 const routes = [{
-        path: '/',
-        name: 'signup',
-        component: SignUpView
-    },
-    {
-        path: '/main',
-        name: 'main',
-        component: MainView
-    },
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+},
+{
+    path: "/login",
+    name: "LogIn",
+    component: LogIn,
+},
+{
+    path: '/',
+    name: 'main',
+    component: MainView,
+    beforeEnter: async (to, from, next) => {
+        let authResult = await auth.authenticated();
+        if (!authResult) {
+            next('/login')
+        } else {
+            next();
+        }
+    }
+},
 ]
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes
-  });
-  
+});
+
 export default router;
